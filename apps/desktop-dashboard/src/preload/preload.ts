fd1bfbcacdb7ee246decc31799f76a4964e98fdd
MJ Mazzei <nuglifeclub@gmail.com>
2025-11-10 19:20:47 -0800

    Revert "chore(preload): log secure-volume api exposure"
    This reverts commit e3864867c87fd76829edb08e80b36bd804181ab5.

--- apps/desktop-dashboard/src/preload/preload.ts
+++ apps/desktop-dashboard/src/preload/preload.ts
@@ -26,8 +26,6 @@ const secureVolume = {
     ipcRenderer.invoke(SECURE_VOLUME_IPC_CHANNEL.getPasscode, identity)
 };
 
-console.log('[preload] secureVolume API exposed', Object.keys(secureVolume));
-
 const nativeBridge = {
   openPath: async (target: string): Promise<string> => ipcRenderer.invoke(NATIVE_IPC_CHANNEL.openPath, target)
 };
