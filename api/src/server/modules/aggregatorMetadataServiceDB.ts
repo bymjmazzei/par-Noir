30585814b7ca757160666ca03b30e779d0101215
MJ Mazzei <nuglifeclub@gmail.com>
2025-11-16 10:32:23 -0800

    Fix SQL authorDid filter: add explicit NULL checks before equality comparisons

--- api/src/server/modules/aggregatorMetadataServiceDB.ts
+++ api/src/server/modules/aggregatorMetadataServiceDB.ts
@@ -148,9 +148,9 @@ export class AggregatorMetadataServiceDB {
 
       if (filters?.authorDid) {
         query += ` AND (
-          am.metadata->'creator'->'identifier'->>'value' = $${paramIndex} OR
-          am.metadata->'creator'->>'@id' = $${paramIndex} OR
-          am.metadata->'author'->>'did' = $${paramIndex}
+          (am.metadata->'creator'->'identifier'->>'value' IS NOT NULL AND am.metadata->'creator'->'identifier'->>'value' = $${paramIndex}) OR
+          (am.metadata->'creator'->>'@id' IS NOT NULL AND am.metadata->'creator'->>'@id' = $${paramIndex}) OR
+          (am.metadata->'author'->>'did' IS NOT NULL AND am.metadata->'author'->>'did' = $${paramIndex})
         )`;
         params.push(filters.authorDid);
         paramIndex++;
