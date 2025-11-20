711379e070590786bb8d56d8a5299511528e2e14
MJ Mazzei <nuglifeclub@gmail.com>
2025-11-19 22:37:18 -0800

    fix: Add console.log directly in JSX to verify CommentModal render path

--- apps/aggregator-browser/src/App.tsx
+++ apps/aggregator-browser/src/App.tsx
@@ -3307,16 +3307,19 @@ function App() {
         )}
 
         {/* Comment Modal - Render outside conditional views to ensure it works on all pages */}
-        {commentingFile && (
-          <CommentModal
-            key={commentingFile.metadata.fileId} // Force remount on file change
-            file={commentingFile}
-            onClose={() => {
-              console.log('[App] CommentModal onClose called', { viewingCreatorId, viewMode });
-              setCommentingFile(null);
-            }}
-          />
-        )}
+        {commentingFile ? (
+          <>
+            {console.log('[App] RENDERING CommentModal NOW', { fileId: commentingFile.metadata.fileId })}
+            <CommentModal
+              key={commentingFile.metadata.fileId} // Force remount on file change
+              file={commentingFile}
+              onClose={() => {
+                console.log('[App] CommentModal onClose called', { viewingCreatorId, viewMode });
+                setCommentingFile(null);
+              }}
+            />
+          </>
+        ) : null}
 
         {/* Edit File Modal */}
         {editingFile && (
